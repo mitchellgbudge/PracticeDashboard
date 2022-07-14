f07cbf97070de1be45f5138914a9e3514cde7cce
Mitchell <mitchellbudge@gmail.com>
2022-07-14 10:55:35 -0700

    Stage initial project

---
+++ PracticeDashboard/PracticeDashboard/PracticeDashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeDashboardApp.swift
+//  PracticeDashboard
+//
+//  Created by Mitchell Budge on 7/14/22.
+//
+
+import SwiftUI
+
+@main
+struct PracticeDashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
